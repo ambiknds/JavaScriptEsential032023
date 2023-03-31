dca7f3389dcea2d6e9ea1f23a47c8b46e1a465a3
Ambition Kyndiah <53797801+ambiknds@users.noreply.github.com>
2023-03-31 06:13:25 +0000

    project add div to display temp ant weather type

--- Project/script.js
+++ Project/script.js
@@ -1,10 +1,17 @@
 function getData() {
-  var cityName = document.getElementById('city').value;
-  alert(cityName);
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=9a523cebee2fa8a5873481b16344510d";
-  fetch(url).then((res)=>{
-   res.json().then(()=>{
-    console.log(res.body);
-});
-});
+  let cityName = document.getElementById('city').value;
+  // alert(cityName);
+  let temp = "";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&appid=9a523cebee2fa8a5873481b16344510d";
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data);
+      var temp =  data.main.temp ;
+      temp = temp + " Degree Celsius";
+      var weatherType = data.weather[0].description
+      document.getElementById('temperature').innerText = temp;
+      document.getElementById('weatherType').innerText = weatherType ;
+  });
+  
 }
